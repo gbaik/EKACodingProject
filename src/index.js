050b2674c0600a7d13bae28c5a99388981332d9f
Gideon Baik <gideonbaik@gmail.com>
2018-01-31 19:32:21 -0800

    Cleanup client index.js parenthesis

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux"
-import { Provider } from "react-redux"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
 
-import reducers from "./stores/reducers.js"
-import middleware from "./stores/middleware";
-import Router from "./components/router";
+import reducers from './stores/reducers.js'
+import middleware from './stores/middleware';
+import Router from './components/router';
 
 const store = createStore(reducers, middleware);
+
 const App = () => (
   <Provider store={store}> 
     <BrowserRouter>
@@ -17,4 +18,4 @@ const App = () => (
   </Provider>
 )
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
