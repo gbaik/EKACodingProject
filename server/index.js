a028be523caca9455f4ccb4c25156f8e8b3e9b98
Gideon Baik <gideonbaik@gmail.com>
2018-01-31 16:40:21 -0800

    Fix server code to render static files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '/../dist')));
 
 app.get('/', function (req, res) {
   res.end();
